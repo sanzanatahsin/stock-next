bd5e2738b988ce921b032a0a68242edde285701d
SanzanaTahsin <u6138302@au.edu>
2023-02-05 01:44:23 +0700

    init

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+
+export default function AboutPage() {
+  return (
+    <>
+    <Head>
+      <title>About Page</title>
+    </Head>
+    <h1>About Page</h1>
+    <p>
+      About Sanzana.
+    </p>
+    <Link href="/">Home</Link>
+    </>
+  )
+}
